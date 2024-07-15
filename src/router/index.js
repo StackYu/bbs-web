327988eee9838e130c8cc55d617e1acd790628c7
xiaoyu <jiangzhong91@gmail.com>
2024-07-15 15:21:42 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -59,7 +59,11 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '@/views/login')
+        component: () => import(/* webpackChunkName: "about" */ '@/views/login'),
+        meta:{
+            title:'用户登录',
+            icon:'mdi-login-variant'
+        }
     },
     {
         path: '/register',
